e69e913d4fad517de08c6933bc60f30074f06370
Matheus Dresch <contato.matheusdresch@gmail.com>
2021-10-16 11:45:03 -0300

    Folder 'scripts' renamed

--- Scripts/notaPage.js
+++
@@ -1,8 +0,0 @@
-function calc() {
-    const primeiraNota = Number(document.getElementById('nota1').value)
-    const segundaNota = Number(document.getElementById('nota2').value)
-    const terceiraNota = Number(document.getElementById('nota3').value)
-
-    let media = Number((primeiraNota+segundaNota+terceiraNota)/3)
-    document.getElementById('resultDisplay').value = media.toFixed(2)
-}
